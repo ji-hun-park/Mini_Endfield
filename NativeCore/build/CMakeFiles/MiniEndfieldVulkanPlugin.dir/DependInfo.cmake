7315eab1f58531d193c06e0afc1ac7c4af825bd5
JiHun <pgh979@gmail.com>
2026-08-26 16:19:55 +0900

    add plugin

---
+++ NativeCore/build/CMakeFiles/MiniEndfieldVulkanPlugin.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/ArrFolder/WorkSpaces/UnityWorkSpace/Mini_Endfield/NativeCore/Src/PluginAPI.cpp" "CMakeFiles/MiniEndfieldVulkanPlugin.dir/Src/PluginAPI.cpp.obj" "gcc" "CMakeFiles/MiniEndfieldVulkanPlugin.dir/Src/PluginAPI.cpp.obj.d"
+  "D:/ArrFolder/WorkSpaces/UnityWorkSpace/Mini_Endfield/NativeCore/Src/VulkanBackend.cpp" "CMakeFiles/MiniEndfieldVulkanPlugin.dir/Src/VulkanBackend.cpp.obj" "gcc" "CMakeFiles/MiniEndfieldVulkanPlugin.dir/Src/VulkanBackend.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
